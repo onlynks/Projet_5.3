a77787c30f3c461eb54ea91bfcda17e6d5d07dd6
onlynks <nicogar12@gmail.com>
2017-10-23 16:27:59 +0000

    router implementation

--- Framework/Route.php
+++ Framework/Route.php
@@ -5,4 +5,33 @@ namespace Framework;
 class Route
 {
     public static $routes = [];
+    
+    
+    public static function hydrate( $routesAdded = [])
+    {
+        self::$routes = $routesAdded;
+    }
+    
+    public static function matchAndDisptch( $currentRoute )
+    {
+        
+       //Array creation from URL
+       $data = explode('/',$currentRoute);
+       $shortcut = array_slice($data,3,4);
+       
+       //Swap to associative array
+       $CurrentRouteShortcut = array('entity' => $shortcut[0], 'action' => $shortcut[1]);
+       
+       if(in_array($CurrentRouteShortcut,self::$routes))
+       {
+          $controllerClass = 'App\Controller\\' . ucfirst($CurrentRouteShortcut['entity']) . 'Controller';
+          $controllerClass::defineAction($CurrentRouteShortcut['action']);
+       }
+       else
+       {
+           echo 'La route n\'existe pas';
+       }
+       
+    }
 }
+
--- Public/index.php
+++ Public/index.php
@@ -1,13 +1,20 @@
 <?php
-
+//define the main file
 define( "APP_PATH", dirname( dirname( __FILE__ ) ) );
 
 // Load and use the Autoloader
-
 include ('../Framework/Autoloader.php');
 Framework\Autoloader::Register();
 
-Framework\Router::drive();
+//hydrate routes from the configuration file
+Framework\Route::hydrate(App\Config\Routes::$routes);
+
+//check if the current URL match with one of our routes, if it does, dispatch to the right controller
+Framework\Route::matchAndDisptch($_SERVER[REQUEST_URI]);
+
+
+
+
 
 
 
